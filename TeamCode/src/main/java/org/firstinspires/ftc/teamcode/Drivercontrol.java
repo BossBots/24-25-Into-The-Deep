f4c54511c3fd58ee896f91fbc6d2af3073fc7543
Adam <zhengkaitaken@gmail.com>
2024-10-03 17:19:53 -0700

    Revamped Drivercontrol.java with new controls

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Drivercontrol.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Drivercontrol.java
@@ -7,17 +7,15 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
+
 @TeleOp
 public class Drivercontrol extends LinearOpMode {
 
     private DcMotor linearSlideMotor;
     private Mecanum mecanum;
     private Servo clawAngle;
-
     private Servo openClaw;
-
     private Servo launcher;
-    //private Servo joint;
     private boolean currentState;
     private boolean droneState;
     private boolean oldState = false;
@@ -27,10 +25,9 @@ public class Drivercontrol extends LinearOpMode {
     private double abortAngle = 0.75;
     private double levelAngle = 0.7272;
 
-    //private double storePix = 0.25;
     @Override
     public void runOpMode() throws InterruptedException {
-
+        // Initialize hardware (keep your existing initialization code here)
         mecanum = new Mecanum(
                 hardwareMap.get(BHI260IMU.class, "imu"),
                 hardwareMap.get(DcMotor.class, "frontLeft"),
@@ -54,102 +51,94 @@ public class Drivercontrol extends LinearOpMode {
         waitForStart();
 
         while (opModeIsActive()) {
-            //linear  slide controls
-            if (Math.abs(gamepad2.right_stick_y)<0.1) {
+            // Linear slide controls
+            if (Math.abs(gamepad2.right_stick_y) < 0.1) {
                 linearSlideMotor.setPower(0);
                 linearSlideMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
             } else {
-                linearSlideMotor.setPower(0.25 * 0.75*gamepad2.right_stick_y);
+                linearSlideMotor.setPower(0.25 * 0.75 * gamepad2.right_stick_y);
             }
 
-            //claw 0.25 is level, 0.25 - 0 is dropping off pixel, 0.25> is facing down
-//            currentState= gamepad2.a;
-//            if(currentState) {
-//                clawAngle.setPosition(0.35);
-//            }
-
-            //open
-            currentState = gamepad2.dpad_down;
-            if(currentState) {
-                if(openClaw.getPosition() != 0.1)
-                    {openClaw.setPosition(0.1);}
+            // Claw open/close controls
+            if (gamepad2.dpad_down) {
+                if (openClaw.getPosition() != 0.1) {
+                    openClaw.setPosition(0.1);
+                }
             }
-            //close
-            currentState = gamepad2.dpad_up;
-            if(currentState){
-                if(openClaw.getPosition() != 0.04)
-                    {openClaw.setPosition(0.04);}
+            if (gamepad2.dpad_up) {
+                if (openClaw.getPosition() != 0.04) {
+                    openClaw.setPosition(0.04);
+                }
             }
 
-
-            droneState = gamepad2.b;
-            if(droneState){
+            // Drone launcher control
+            if (gamepad2.b) {
                 launcher.setPosition(0.95);
             }
 
+            // Claw angle controls
             double currentAngle;
-//            while (Math.abs(gamepad2.right_stick_x)>0.1) {
-//                clawPosition = claw.getPosition();
-//                claw.setPosition(clawPosition-0.001);
-//                claw.wait(10);
-//            }
-
-            while((gamepad2.left_stick_y)>0.1){
+            if (gamepad2.left_stick_y > 0.1) {
                 currentAngle = clawAngle.getPosition();
                 clawAngle.setPosition(currentAngle + 0.00025);
-                }
-            while((gamepad2.left_stick_y)<-0.1){
+            }
+            if (gamepad2.left_stick_y < -0.1) {
                 currentAngle = clawAngle.getPosition();
-                if(currentAngle > abortAngle){
+                if (currentAngle > abortAngle) {
                     clawAngle.setPosition(levelAngle);
+                } else if (currentAngle <= abortAngle) {
+                    clawAngle.setPosition(currentAngle - 0.00025);
                 }
-                else if (currentAngle <= abortAngle){
-                    clawAngle.setPosition(currentAngle - 0.00025); //negative equals turning counterclockwise for now
-                }
-
             }
 
-//            currentState= gamepad2.b;
-            //not used right now
-
-//            if(currentState){
-//                joint.setPosition(0.1); //twist left hypothetically
-//            }
-//            else{
-//                joint.setPosition(0.3); //twist right hypothetically
-//            }
-
-//            (!oldState && currentState){
-//                clawAction = !clawAction;
-//            }
-//            oldState= currentState;
-//            if(clawAction){
-//                claw.setPosition(0.4); // closed
-//            } else {
-//                claw.setPosition(0.1); // open
-//            } */
-            //driving
-
+            // Precision mode toggle
             newState = gamepad1.a;
             if (newState && !oldState) {
                 precisionMode = !precisionMode;
             }
             oldState = newState;
 
-            if (gamepad1.x) {
-                mecanum.brake(10);
-            } else if (precisionMode) {
-                mecanum.drive( 0.5 * (gamepad1.right_trigger - gamepad1.left_trigger),
-                        0.5 * (gamepad1.left_stick_x), 0.5 * (gamepad1.right_stick_x));
-            } else {
-                mecanum.drive(gamepad1.right_trigger - gamepad1.left_trigger,
-                        gamepad1.left_stick_x, gamepad1.right_stick_x);
+            // Updated driving controls
+            double drive = -gamepad1.left_stick_y; // Forward/backward
+            double strafe = gamepad1.left_stick_x; // Left/right movement
+            double rotate = 0;
+
+            // Rotation controls
+            if (gamepad1.right_trigger > 0) {
+                rotate = gamepad1.right_trigger; // Clockwise rotation
+            } else if (gamepad1.left_trigger > 0) {
+                rotate = -gamepad1.left_trigger; // Counter-clockwise rotation
             }
 
+            // Strafe controls
+            if (gamepad1.right_bumper) {
+                strafe = 1.0; // Strafe right
+            } else if (gamepad1.left_bumper) {
+                strafe = -1.0; // Strafe left
+            }
+
+            // Apply precision mode if active
+            if (precisionMode) {
+                drive *= 0.5;
+                strafe *= 0.5;
+                rotate *= 0.5;
+            }
+
+            // Drive the robot
+            if (gamepad1.x) {
+                mecanum.brake(10);
+            } else {
+                mecanum.drive(drive, strafe, rotate);
+            }
+
+            // Telemetry
             telemetry.addData("precision mode", precisionMode);
             telemetry.addData("current angle", clawAngle.getPosition());
             telemetry.addData("current angle of open close", openClaw.getPosition());
             telemetry.addData("current position of the launcher servo", launcher.getPosition());
+            telemetry.addData("Drive", drive);
+            telemetry.addData("Strafe", strafe);
+            telemetry.addData("Rotate", rotate);
             telemetry.update();
         }
     }
